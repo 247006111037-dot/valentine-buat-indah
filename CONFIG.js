3ac1c93d0dbc0923f3f8726d41241f0cc6021b1d
247006111037-dot <247006111037@student.unsil.ac.id>
2026-02-14 13:06:09 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Happy Valentine's Day, Indah. </br> Ada sesuatu yang mau aku sampaikan...",
+  descriptionHeader:
+    "Jawab jujur ya. Kalau kamu diam atau tutup halaman ini, aku anggap jawabannya 'Iya'.",
+  buttonYes: "Iya, mau",
+  buttonNo: "Enggak",
+  titleModar: "Terima kasih, Indah.",
+  descriptionModar:
+    "Makasih sudah kasih aku kesempatan buat jadi bagian dari hari-hari kamu. </br> Aku nggak janji bakal sempurna, tapi aku janji bakal selalu ada buat kamu. Kamu berarti banget buat aku. </br> Nanti kalo aku udah ketasik lagi kita keluar yuk bisa kan? Kita cari suasana baru sebentar.I LOVE U </br> See you soon. <3",
+};
